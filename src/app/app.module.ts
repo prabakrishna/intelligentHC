a6fa0d4509e33f8bcc2896502eabd5ab3129ead3
prabakrishna <prabamsec4@gmail.com>
2018-08-12 16:53:24 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppComponent } from './app.component';
 import { ContainerModuleComponent } from './container-module/container-module.component';
 import { IterationModuleComponent } from './iteration-module/iteration-module.component';
 import {MyServiceService} from './my-service.service';
+import { EcorrModuleComponent } from './ecorr-module/ecorr-module.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContainerModuleComponent,
-    IterationModuleComponent
+    IterationModuleComponent,
+    EcorrModuleComponent
   ],
   imports: [
     BrowserModule,
--- src/app/iteration-module/iteration-module.component.ts
+++ src/app/iteration-module/iteration-module.component.ts
@@ -2,15 +2,49 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import {Ecorr} from '../Models/Ecorr.model';
 import { FalloutAccount } from '../Models/Fallout.model';
 import {ActivatedRoute} from '@angular/router';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-iteration-module',
   templateUrl: './iteration-module.component.html',
-  styleUrls: ['./iteration-module.component.css']
+  styleUrls: ['./iteration-module.component.css'],
+  animations:[
+   trigger('ecorr-div', [
+     state('hide', style({
+       transform:'translateX(-100px)',
+       opacity:0
+           
+     })),
+     state('show', style({
+      'background-color':'rgb(204, 230, 247)',
+       transform:'translateX(0px)'            
+     })),
+     transition('*=>*',animate(300))
+
+   ]),
+
+   trigger('fallout-div', [
+    state('hide', style({
+      transform:'translateX(-100px)',
+      opacity:0
+          
+    })),
+    state('show', style({
+     'background-color':'rgb(204, 230, 247)',
+      transform:'translateX(0px)'            
+    })),
+    transition('*=>*',animate(300))
+
+  ])
+  ]
 })
 export class IterationModuleComponent implements OnInit {
 //@ViewChild('iterationSelected') iterationSelected : ElementRef;
 iterationId;
+ecorrView = 'false';
+ecorrViewStatus='hide';
+falloutView = 'false';
+falloutViewStatus = 'hide'
  ecorrAccounts : Ecorr[] = [
   new Ecorr('1234567','1234234510001','OB041','12','5000','3800'),
   new Ecorr('1234568','3434234510001','OB211','5','89000','84000'),
@@ -43,7 +77,18 @@ this.route.params.subscribe(res=>this.iterationId=res.id);
   showAccounts(){
     console.log("inside");
       console.log(this.ecorrAccounts.length);
+  }
+
+  showEcorr(){
+    this.ecorrViewStatus=='hide'?this.ecorrViewStatus='show': this.ecorrViewStatus='hide';
+    this.ecorrView=='true'?this.ecorrView='false': this.ecorrView='true';
+
+  }
+
   
+  showFallout(){
+    this.falloutViewStatus=='hide'?this.falloutViewStatus='show': this.falloutViewStatus='hide';
+    this.falloutView=='true'?this.falloutView='false': this.falloutView='true';
 
   }
 }
